e4e7e29ae817ed7c4e4d9a60a1f4a1e542aa1232
maheen <maheenzuhra@gmail.com>
2025-08-23 15:13:38 +0500

    make pricing and about section updated

--- script.js
+++ script.js
@@ -52,34 +52,3 @@ backgroundSlide();
 
 setInterval(backgroundSlide, 3000);
 
-//  for counter section
-
-function startCount(element, target, suffix) {
-  let count = 0;
-  let duration = 2000;
-  let step = Math.ceil(target / (duration / 20));
-  let countInterval = setInterval(() => {
-    count += step;
-    element.textContent = count + suffix;
-
-    if (count >= target) {
-      clearInterval(countInterval);
-    }
-  }, 20);
-}
-
-let started = false;
-
-window.addEventListener("scroll", function () {
-  let counterSection = document.querySelector(".counter");
-  let position = counterSection.getBoundingClientRect().top;
-  let screenHeight = this.window.innerHeight;
-
-  if (position < screenHeight && !started) {
-    startCount(document.querySelector("#counter1"), 200, "+");
-    startCount(document.querySelector("#counter2"), 97, "%");
-    startCount(document.querySelector("#counter3"), 5628, "+");
-    startCount(document.querySelector("#counter4"), 5637, "+");
-    started = true;
-  }
-});
